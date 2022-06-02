8f04d23e571ea79241a8a0ae0e3d13cec5d7c487
Borja Gómez Muñoz <borja@reverscore.com>
2022-06-02 13:31:38 +0200

    Created is-deforested endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import sys
 sys.path.insert(1, './src')
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 import logging
 from logging.config import dictConfig
@@ -12,6 +13,18 @@ dictConfig(log_config)
 logger = logging.getLogger("capstone") # should be this name unless you change it in log_config.py
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/healthcheck', status_code=status.HTTP_200_OK)
 def perform_healthcheck():
@@ -26,3 +39,11 @@ async def main():
 </body>
     """
     return HTMLResponse(content=content)
+
+@app.get("/v1/is-deforested")
+async def isDeforested(lat: float = None, lng: float = None):
+    if not lat or not lng:
+        raise HTTPException(status_code=400, detail="lat and lng are required")
+    else:
+        return {'isDeforested': 'Ok'}
+
