783133242a1fa8cea05643fefad3bf0072fac09d
Borja Gómez Muñoz <borja@reverscore.com>
2022-06-07 23:06:33 +0200

    Fixing DB connection close

--- src/controllers/deforestation.py
+++ src/controllers/deforestation.py
@@ -2,15 +2,14 @@ import os
 import psycopg2
 import json
 
-print(str(os.environ.keys()))
-
-conn = psycopg2.connect(
-    host=os.environ['DB_URL'],
-    database=os.environ['POSTGRES_DB'],
-    user=os.environ['POSTGRES_USER'],
-    password=os.environ['POSTGRES_PASSWORD'])
-
 def isDeforested(lat: float, lng: float):
+
+  conn = psycopg2.connect(
+      host=os.environ['DB_URL'],
+      database=os.environ['POSTGRES_DB'],
+      user=os.environ['POSTGRES_USER'],
+      password=os.environ['POSTGRES_PASSWORD'])
+      
   cur = conn.cursor()
   query = """SELECT * FROM prediction WHERE 
   sqbl_longitude <= %s AND sqbl_latitude <= %s AND 
@@ -20,6 +19,8 @@ def isDeforested(lat: float, lng: float):
   
   cur.execute(query, (lng, lat, lng, lat))
   rows = cur.fetchall()
+
+  conn.close()
 
   if len(rows) == 0:
     return None 
